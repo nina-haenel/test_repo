f35470793087dc9980124e9b9a4d8d46bddb0eee
nina-haenel <nina.haenel@gesis.org>
2025-07-02 11:13:12 +0200

    Installing libraries

---
+++ test_script.R
@@ -0,0 +1,10 @@
+## hello, this is me trying some things.
+install.packages("tidyr")
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("haven")
+install.packages("foreign")
+install.packages("here")
+install.packages("xlsx")
+
